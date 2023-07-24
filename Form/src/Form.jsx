ae30ace180c16da476979e1dfed90be77bc09637
Tachy-22 <entekumejeffrey@gmail.com>
2023-07-24 19:15:01 +0100

    meh

--- Form/src/Form.jsx
+++ Form/src/Form.jsx
@@ -5,45 +5,50 @@ const Form = () => {
 
   const handleChange = (event) => {
     const value = event.target.value;
-    const placeHolder = event.target.placeholder;
+    const Name = event.target.name;
     setData((prevData) => ({
       ...prevData,
-      [placeHolder]: value,
+      [Name]: value,
     }));
   };
   console.log(data);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("submitted");
+    const formEle = document.querySelector("form");
+    const formData = new FormData(formEle);
+    console.log(formData);
+    const stringed = JSON.stringify(data);
+    const parsed = JSON.parse(stringed);
 
     const url =
-      "https://script.google.com/macros/s/AKfycbyqra4o2VMftEtITV9Tf8wPvCM02vL_HC6mOJhkOc__1MlMzeiYGM3qR88xX7Hijw0YuQ/exec";
+      "https://script.google.com/macros/s/AKfycbw1PAt5RFRfi9fd53fJRXXvqVY4YDP0U1AFIqvkzB4_SxvnrRe_6AR0h8pfxjSbuM6u9g/exec";
 
     try {
+      console.log(stringed, parsed, "john");
       const response = await fetch(url, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Set the appropriate content type
-        },
-        body: JSON.stringify(data), // Convert the data object to JSON before sending
+        body: stringed,
       });
 
-      // Check if the request was successful (status code 200-299)
       if (response.ok) {
-        console.log("Data sent successfully!");
+        // Read the response body as JSON
+        const responseData = await response.text();
+        console.log("Response JSON:", responseData, response);
       } else {
         console.log("Failed to send data.");
       }
     } catch (error) {
       console.error("Error while sending data:", error);
-      setData({ Name: "", Email: "", Message: "" });
+      // setData({ Name: "", Email: "", Message: "" });
     }
   };
+
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
         <span className="heading">Get in touch</span>
         <input
+          name="Name"
           placeholder="Name"
           type="text"
           className="input"
@@ -51,6 +56,7 @@ const Form = () => {
           onChange={handleChange}
         />
         <input
+          name="Email"
           placeholder="Email"
           id="mail"
           type="email"
@@ -59,11 +65,11 @@ const Form = () => {
           onChange={handleChange}
         />
         <textarea
+          name="Message"
           placeholder="Message"
           rows="10"
           cols="30"
           id="message"
-          name="message"
           className="textarea"
           value={data.Message}
           onChange={handleChange}
